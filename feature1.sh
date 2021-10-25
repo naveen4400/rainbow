0706d1f917b3b26146d6175c271cd13f19e91c2a
naveen <daramnaveen12@gmail.com>
2021-10-24 22:17:56 -0700

    change

--- feature1.sh
+++ feature1.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Add two numeric value
-((sum=25+35))
 
-#Print the result
-echo $sum
+valid=true
+count=1
+while [ $valid ]
+do
+echo $count
+if [ $count -eq 5 ];
+then
+break 
+fi
+((count++))
+done
--- file4.sh
+++ file4.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-:'
-the followimg script calculates
-the squre value of the number ,5.
-'
+
+
 ((area=5*5))
 echo $area
