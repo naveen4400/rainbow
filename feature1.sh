edc4186e8affbc277786fa8cf5f1928cdf58ba04
indian116 <93109551+indian116@users.noreply.github.com>
2021-10-25 09:31:48 +0530

    update feature1

---
+++ feature1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+year='date +%Y'
+month='month +%M'
+day='day +%d'
+hour='hour +%H'
+minute='minute +%M'
+second='secpnd +%S'
+echo 'date'
+echo "current date is : $day-$month-$year"
+echo "current time is :$hour-$minute-$second"
