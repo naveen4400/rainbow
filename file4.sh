623a92dc69c3c4b16f980bb98b878cf42085a3ab
naveen <daramnaveen12@gmail.com>
2021-11-28 22:45:54 -0800

    file4

--- file4.sh
+++ file4.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-n=10
-if [ $n -lt 10 ];
-then
-echo "It is a one digit number"
-else
-echo "It is a two digit number"
-fi
+# get the cuurent system date and time vlaue using 'date' command.
+# every part of date and time value can be parsed using 'Y','m','d','H','M' amd 'S'.
+
+Year='date +%Y'
+Month='date +%m'
+Day='date +%d'
+Hour='date +%H'
+Minute='date +%M'
+Second='date +%S'
+echo 'date'
+echo "Current Date is: $Day-$Month-$Year"
+echo "current Time is: $Hour-$Minute:$Second"
+
